ee07df8535c24f19fb16d30b4438aa6b8ef4e7d9
David Barrero <davbarfer@gmail.com>
2024-02-12 23:08:33 +0100

    Start project

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'node:url'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  define: { 'process.env': {} },
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
+    }
   },
   server: {
-    port: 3000,
-  },
+    port: 3000
+  }
 })
