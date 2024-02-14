0df1489156b9501a8cc1f946d1e682f3ff9dc99c
David Barrero <davbarfer@gmail.com>
2024-02-14 11:04:24 +0100

    Update description

--- src/helpers/main.ts
+++ src/helpers/main.ts
@@ -5,14 +5,14 @@ interface AboutMe {
 export const aboutMeHome:AboutMe[] = [
     {
         field: "Personal",
-        description: "I considered my self someone who ...."
+        description: "I consider myself someone willing to create a positive impact on my environment. In my opinion, when someone shows their best, people around will be influenced by it. That is how I intend to live my life."
     },
     {
         field: "Studies",
-        description: "I started the university back in 2019 ... "
+        description: " I started the university back in 2019. Due to different recommendations, I found myself pursuing a degree in engineering. Industrial Engineering grew in me as this journey went by, becoming my passion."
     },
     {
         field: "Hobbies",
-        description: "Why do not follow the things I have passion for? Like medieval things."
+        description: "Engineering isn't everything in my life. I also invest my time in various topics, such as medieval things and travelling."
     }
 ];
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -7,12 +7,12 @@ import { aboutMeHome } from '@/helpers/main';
     <section
     v-for="(info,i) in aboutMeHome"
     :key="i"
-    class="flex flex-col justify-start items-center m-2"
+    class="flex flex-col justify-start items-center m-4"
     >
-    <div class="bg-zinc-600 p-2 rounded-xl min-w-40 flex justify-center">
+    <div class="bg-zinc-600 p-2 rounded-full min-w-40 flex justify-center">
         <h1 class="text-2xl font-bold">{{ info.field }}</h1>
     </div>
-        <p class="m-2 text-center">{{ info.description }}</p>
+        <p class="m-2 text-justify">{{ info.description }}</p>
     </section>
 </div>
 </template>
