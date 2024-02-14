c8ff7f31236ae68f40c630f925150ed6faffd308
David Barrero <davbarfer@gmail.com>
2024-02-14 11:10:28 +0100

    Update Website Structure

--- src/components/CareerVue.vue
+++ src/components/CareerVue.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
+import EstiemVue from './EstiemVue.vue';
 </script>
 
 <template>
     <section>
         <div class="font-bold">Career</div>
+        <EstiemVue />
     </section>
 </template>
