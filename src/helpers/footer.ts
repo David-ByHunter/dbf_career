b2b58447ba553a8b8c4d4cbf300c1771f98f503a
David Barrero <davbarfer@gmail.com>
2024-02-14 11:31:30 +0100

    Basic footer

---
+++ src/helpers/footer.ts
@@ -0,0 +1,16 @@
+interface footerInfo {
+    info: string,
+    icons: string[],
+    linkIcons: string[]
+}
+export const infoFooter: footerInfo = {
+    info: "Created by David Barrero",
+    icons: [
+        "fab fa-linkedin",
+        "fab fa-instagram"
+    ],
+    linkIcons: [
+        "https://www.linkedin.com/in/davidbarfer",
+        "https://www.instagram.com/david_byhunter/",
+    ]
+} 
--- src/views/FooterView.vue
+++ src/views/FooterView.vue
@@ -1,23 +1,20 @@
 <script setup lang="ts">
-const icons = [
-    "fab fa-linkedin",
-    "fab fa-instagram"
-]
-const info = [
-    "https://www.linkedin.com/in/davidbarfer",
-    "https://www.instagram.com/david_byhunter/",
-]
+import { infoFooter } from '@/helpers/footer'
 </script>
 
 <template>
-    <footer class="flex justify-end">
+    <footer class="flex justify-end items-center">
+        <span class="italic font-light">
+            {{ infoFooter.info }}
+        </span>
         <a
-            v-for="(ref,i) in info"
+            v-for="(ref,i) in infoFooter.linkIcons"
             :key="i"
             :href="ref"
             target="_blank"
+            class="hover:opacity-80"
         >
-            <font-awesome-icon class="m-6 size-8" :icon="icons[i]" />
+            <font-awesome-icon class="m-2 size-8" :icon="infoFooter.icons[i]" />
         </a>
     </footer>
 </template>
