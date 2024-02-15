733d2bb0475c0a5eb86582a7388a5b3b324667d3
David Barrero <davbarfer@gmail.com>
2024-02-15 10:12:37 +0100

    Create Not found page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainView from '../views/MainView.vue'
 import Career from '@/components/CareerVue.vue'
-import MedievalThings from '@/components/MedievalThings.vue'
+// import MedievalThings from '@/components/MedievalThings.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -11,15 +11,20 @@ const router = createRouter({
       component: MainView
     },
     {
-      path:'/medievalthings',
-      name: 'Medieval',
-      component: MedievalThings
-    },
-    {
       path: '/career',
       name: 'Career',
       component: Career
-    }
+    },
+    // {
+    //   path:'/medievalthings',
+    //   name: 'Medieval',
+    //   component: MedievalThings
+    // },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: () => import("@/components/NotFound.vue"),
+    },
   ]
 })
 
