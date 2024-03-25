667861e54cd9233d4a9ef201136bdc1de65a08ed
David Barrero <davbarfer@gmail.com>
2024-03-25 13:25:59 +0100

    Update bg-color

--- src/components/BackToTopBtn.vue
+++ src/components/BackToTopBtn.vue
@@ -24,6 +24,6 @@ onMounted(() => {
         class="fixed bottom-2 left-2 mb-8"
         @click="scrollTop" v-show="isScroll"
     >
-        <FontAwesomeIcon class="size-6 bg-accent rounded-full p-2" icon="fas fa-chevron-up" />
+        <FontAwesomeIcon class="size-6 bg-secondary rounded-full p-2" icon="fas fa-chevron-up" />
     </button>
 </template>
