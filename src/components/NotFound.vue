43b546712bfa89b9e36f5f3b15a70525645da7f9
David Barrero <davbarfer@gmail.com>
2024-03-25 17:58:45 +0100

    Change style

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -3,10 +3,10 @@
 
 <template>
     <section class="flex flex-col justify-center items-center *:m-4">
-        <h1 class="text-2xl font-bold uppercase bg-accent min-w-40 p-4 rounded-full">page not found</h1>
+        <h1 class="text-2xl font-bold uppercase text-primary bg-accent min-w-40 p-4 rounded-full">error 404: page not found</h1>
         <button
             @click="$router.push({name: 'Home'})"
-            class="p-4 bg-accent hover:opacity-60 min-w-40 rounded-full text-2xl font-bold uppercase "
+            class="p-4 bg-accent hover:opacity-60 min-w-40 rounded-full text-2xl font-bold uppercase text-primary"
         >
             go back home
         </button>
