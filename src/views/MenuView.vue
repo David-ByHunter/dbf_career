214760e413d00967a681a1b7f9cb49e6f9024641
David Barrero <davbarfer@gmail.com>
2024-03-25 18:26:23 +0100

    Only icon Menu

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -2,36 +2,17 @@
 import { Menu} from '@/helpers/menu'
 import { iconMenu } from '@/helpers/menu'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { ref } from 'vue';
-const state = ref(false)
-const changeState = () => {
-    state.value = !state.value
-}
 </script>
 
 <template>
     <div class="bg-gradient-to-b from-secondary to-primary rounded-r flex flex-col justify-start items-center ">
-        <button @click="changeState" class="m-4">
-            <font-awesome-icon class="size-8" icon="fas fa-bars" />
-        </button>
-        <nav v-show="!state">
+        <nav>
             <ul class="m-2">
                 <li v-for="(item, i) in Menu"
                     :key="i"
                 >
                     <button @click="$router.push({name: item})" class="m-2 hover:opacity-60">
                             <font-awesome-icon class="size-8" :icon="iconMenu[i]"/>
-                    </button>
-                </li>
-            </ul>
-        </nav>
-        <nav v-show="state">
-            <ul class="m-2">
-                <li v-for="(item, i) in Menu"
-                    :key="i"
-                >
-                    <button @click="$router.push({name: item})" class="min-w-20 rounded-full bg-accent font-bold text-primary hover:opacity-60 m-2 p-2">
-                            {{ item }}
                     </button>
                 </li>
             </ul>
