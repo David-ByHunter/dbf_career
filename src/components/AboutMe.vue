46ea61929de6a0ae1d30973d63def48ef8c1e7d2
David Barrero <davbarfer@gmail.com>
2024-07-31 21:59:41 +0200

    Basic Description

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { aboutMe } from '@/helpers/main';
+
+</script>
+<template>
+    <section class="flex flex-col justify-start items-left my-4">
+      <span class="font-bold text-xl underline underline-offset-8 decoration-4">
+        {{ aboutMe.field }}
+      </span>
+      <span class="m-4 italic">{{ aboutMe.description }}</span>
+    </section>
+</template>
