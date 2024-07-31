7f01203eb66d3b228b7bcc73323857853315aece
David Barrero <davbarfer@gmail.com>
2024-07-31 19:46:22 +0200

    Add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,22 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
+    'plugin:prettier/recommended',
   ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 'latest',
+  },
+};
